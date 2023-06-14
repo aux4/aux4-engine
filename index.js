67fa85e95e2ce0ed03a9607d2006ad5af609f246
David SG <david@pairingwith.me>
2023-06-14 00:29:44 -0500

    initial version

---
+++ index.js
@@ -0,0 +1,36 @@
+const Config = require("./lib/Config");
+const Parameters = require("./lib/Parameters");
+const Executor = require("./lib/Executor");
+const ExecutorChain = require("./lib/ExecutorChain");
+const Interpreter = require("./lib/Interpreter");
+
+const LogExecutor = require("./lib/executors/LogExecutor");
+const SetParameterExecutor = require("./lib/executors/SetParameterExecutor");
+const ProfileExecutor = require("./lib/executors/ProfileExecutor");
+const PackageExecutor = require("./lib/executors/PackageExecutor");
+const CommandLineExecutor = require("./lib/executors/CommandLineExecutor");
+const EachExecutor = require("./lib/executors/EachExecutor");
+
+const ParameterInterpreter = require("./lib/interpreters/ParameterInterpreter");
+const DefaultInterpreter = require("./lib/interpreters/DefaultInterpreter");
+const PromptInterpreter = require("./lib/interpreters/PromptInterpreter");
+
+const Engine = require("./lib/Engine");
+
+module.exports = {
+  Engine,
+  Config,
+  Executor,
+  ExecutorChain,
+  Interpreter,
+  Parameters,
+  LogExecutor,
+  SetParameterExecutor,
+  PackageExecutor,
+  ProfileExecutor,
+  CommandLineExecutor,
+  EachExecutor,
+  ParameterInterpreter,
+  DefaultInterpreter,
+  PromptInterpreter
+};
